7481a3ce0916af2376934f6808f164f0939ca0c3
NoNoobSaibot <157010652+NoNoobSaibot@users.noreply.github.com>
2024-10-03 01:45:32 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?phprequire 'db/connect.php';require 'global/global.php';require 'global/function.php';$uri = $_SERVER['REQUEST_URI'];if(preg_match('@^(/)$@',$uri,$ARGS)){	include 'controller/main.php';}else if(preg_match('@^/([a-z0-9_-]+)$@',$uri,$ARGS)){	include 'controller/chapter.php';}else if(preg_match('@^/([a-z0-9_-]+)/([a-z0-9_-]+)$@',$uri,$ARGS)){	include 'controller/subchapter.php';}else if(preg_match('@^/([a-z0-9_-]+)/([a-z0-9_-]+)/([a-z0-9_-]+)$@',$uri,$ARGS)){	include 'controller/subchapter.php';	}else{	echo "Некорректный url";}
+<?php
+
+require 'db/connect.php';
+require 'global/global.php';
+require 'global/function.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+
+if(preg_match('@^(/)$@',$uri,$ARGS)){
+	include 'controller/main.php';
+}else if(preg_match('@^/([a-z0-9_-]+)$@',$uri,$ARGS)){
+	include 'controller/chapter.php';
+}else if(preg_match('@^/([a-z0-9_-]+)/([a-z0-9_-]+)$@',$uri,$ARGS)){
+	include 'controller/subchapter.php';
+}else if(preg_match('@^/([a-z0-9_-]+)/([a-z0-9_-]+)/([a-z0-9_-]+)$@',$uri,$ARGS)){
+	include 'controller/subchapter.php';	
+}else{
+	echo "Некорректный url";
+}
